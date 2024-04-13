46ffc6c4591f80ea9e1c309e689a7209e6a3e0b6
AnndreJunior <anndre.junior15@gmail.com>
2024-04-13 12:50:29 -0300

    feat: create controller to get user data

--- AuthApi.Api/Controllers/AuthController.cs
+++ AuthApi.Api/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using AuthApi.Application.UseCases.Auth;
 using AuthApi.Communication.Requests;
 using AuthApi.Communication.Responses;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthApi.Api.Controllers;
@@ -11,11 +12,13 @@ public class AuthController : ControllerBase
 {
     private readonly SignUpUseCase _signUpUseCase;
     private readonly LoginUseCase _loginUseCase;
+    private readonly GetUserDataUseCase _getUserDataUseCase;
 
-    public AuthController(SignUpUseCase signUpUseCase, LoginUseCase loginUseCase)
+    public AuthController(SignUpUseCase signUpUseCase, LoginUseCase loginUseCase, GetUserDataUseCase getUserDataUseCase)
     {
         _signUpUseCase = signUpUseCase;
         _loginUseCase = loginUseCase;
+        _getUserDataUseCase = getUserDataUseCase;
     }
 
     [HttpPost("sign-up")]
@@ -38,5 +41,23 @@ public class AuthController : ControllerBase
         var response = await _loginUseCase.Execute(request);
 
         return Ok(new AuthenticationResponse(response));
+    }
+
+    /// <remarks>
+    /// note: the avatar is a string because it's a public link to user's photo
+    ///</remarks>
+    [Authorize]
+    [HttpGet("me")]
+    [ProducesResponseType(typeof(AuthUserDataResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(AuthUserDataResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetUserData()
+    {
+        var tokenPayload = User.Identity?.Name;
+        var userId = Guid.Parse(tokenPayload ?? "");
+
+        var userData = await _getUserDataUseCase.Execute(userId);
+
+        return Ok(userData);
     }
 }
