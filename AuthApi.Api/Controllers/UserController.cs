370590ea205977f667bab2499ba3c4fe28d2abfc
AnndreJunior <anndre.junior15@gmail.com>
2024-05-14 20:36:00 -0300

    feat: create a controller with a route to upload the avatar

---
+++ AuthApi.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using AuthApi.Application.UseCases.Profile;
+using AuthApi.Communication.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class UserController : ControllerBase
+{
+    private readonly UploadAvatarUseCase _uploadAvatarUseCase;
+
+    public UserController(UploadAvatarUseCase uploadAvatarUseCase)
+    {
+        _uploadAvatarUseCase = uploadAvatarUseCase;
+    }
+
+    [HttpPut("upload-avatar")]
+    [ProducesResponseType(typeof(ProfileUpdateResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> UploadAvatar(IFormFile avatar)
+    {
+        var tokenPayload = User.Identity?.Name;
+        var userId = Guid.Parse(tokenPayload ?? "");
+        var result = await _uploadAvatarUseCase.Execute(avatar, userId);
+
+        return Created(string.Empty, result);
+    }
+}
